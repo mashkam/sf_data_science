313bb44726295d1ab3fa21aab9689b4d8d0dfdf8
mashkam <mashkamikhovich@yandex.ru>
2021-12-12 18:28:53 +0100

    binary search done

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -5,6 +5,32 @@
 import numpy as np
 
 
+def binsearch(number: int = 1) -> int:
+    """Бинарный поиск
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0  # счетчик попыток
+    prdict_number_min = 1
+    prdict_number_max = 101
+
+    while True:
+        count += 1
+
+        predict_number = (prdict_number_max + prdict_number_min) // 2
+        if number > predict_number:
+            prdict_number_min = predict_number
+        elif number < predict_number:
+            prdict_number_max = predict_number
+        else:
+            break  # выход из цикла если угадали
+    return count
+
+
 def random_predict(number: int = 1) -> int:
     """Рандомно угадываем число
 
@@ -24,21 +50,21 @@ def random_predict(number: int = 1) -> int:
     return count
 
 
-def score_game(random_predict) -> int:
+def score_game(predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    # np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(predict(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
@@ -47,4 +73,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(binsearch)
